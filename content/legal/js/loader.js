5eb459998fba6785def9ef49e6b1b04a67a2a962
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-08-18 11:58:57 +0200

    @pdehaan review.

--- content/legal/js/loader.js
+++ content/legal/js/loader.js
@@ -1,6 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Set the current translation content
 function setBody(data) {
   $("#legal-copy").html(data);
+}
+
+function normalizeLocale(lang) {
+  return lang.replace("-", "_");
 }
 
 $(document).ready(function() {
@@ -8,22 +16,18 @@ $(document).ready(function() {
   var lang, defaultLang = "en-US";
   $.get(loop.config.serverUrl, function(data) {
     if (data.hasOwnProperty("i18n")) {
-      lang = data.i18n.lang;
-      defaultLang = data.i18n.defaultLang;
+      lang = normalizeLocale(data.i18n.lang);
+      defaultLang = normalizeLocale(data.i18n.defaultLang);
     }
     if (lang === undefined) {
-      lang = defaultLang;
+      lang = normalizeLocale(defaultLang);
     }
 
-    $.get(lang.replace("-", "_") + ".html")
-      .done(function(data) {
-        setBody(data);
-      })
+    $.get(lang + ".html")
+      .done(setBody);
       .fail(function() {
-        $.get(defaultLang.replace("-", "_") + ".html")
-          .done(function(data) {
-            setBody(data);
-          });
+        $.get(defaultLang + ".html")
+          .done(setBody);
       });
   });
 });
