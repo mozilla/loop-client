d0c56dc55dfdbca91f9c42bf75d03d285c0557e8
Paul Kerr <paulrkerr@gmail.com>
2015-01-08 15:27:32 -0800

    Integrated feedback on first version

--- locale_update.py
+++ locale_update.py
@@ -11,54 +11,55 @@
 # Changes are then pushed back to loop-client.
 ##
 
-import sys
-import os
-import shutil
+import argparse
 import io
+import os
 import re
+import shutil
+import sys
 
-l10n_src = "../loop-client-l10n/l10n/"
-content_dir = "content/"
-index_file_name = "index.html"
+def main(l10n_src, l10n_dst, index_file_name):
+    print "deleting existing l10n content tree:", l10n_dst
+    shutil.rmtree(l10n_dst, ignore_errors=True)
 
-def createLocale(dst_name_list, src_dir):
-    if len(src_dir) < 1 or src_dir[0] == "." or src_dir == 'templates':
-        return dst_name_list
+    print "updating l10n tree from", l10n_src
+
+    def create_locale(src_dir):
+        dst_dir = src_dir.replace('_', '-')
+        shutil.copytree(os.path.join(l10n_src, src_dir), os.path.join(l10n_dst, dst_dir))
+        return dst_dir
     
-    dst_dir = src_dir.replace('_', '-')
-    shutil.copytree(l10n_src + src_dir, content_dir + "l10n/" + dst_dir)
-    dst_name_list.append(dst_dir)
-    return dst_name_list
-
-def main():
-    print "deleting existing l10n content tree"
-    shutil.rmtree(content_dir + "l10n", True)
     locale_dirs = os.listdir(l10n_src)
+    locale_list = [create_locale(x) for x in locale_dirs if x[0] != "." and x != "templates"]
 
-    print "updating l10n tree"
-    locale_list = reduce(createLocale, locale_dirs, [])
+    print "updating locales list in", index_file_name
+    with io.open(index_file_name, "r+") as index_file:
+        index_html = index_file.read()
 
-    print "updating index.html locales list"
-    index_file = io.open(content_dir + index_file_name, "r+")
-    index_html = index_file.read()
-
-    new_content = re.sub('<meta name=(["|\'])locales\\1.*? content=(["|\']).*?\\2.*? />',
-                         '<meta name="locales" content="' + " ".join(locale_list) + '" />',
-                         index_html, 1, re.M | re.S)
-
-    index_file.seek(0)
-    index_file.truncate(0)
-    index_file.write(new_content)
-    index_file.close()
+        new_content = re.sub(
+            '<meta name=(["|\'])locales\\1.*? content=(["|\']).*?\\2.*? />',
+            '<meta name="locales" content="' + ", ".join(locale_list) + '" />',
+            index_html, 1, re.M | re.S)
+        
+        index_file.seek(0)
+        index_file.truncate(0)
+        index_file.write(new_content)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        l10n_src = sys.argv[1]
-        if l10n_src[-1] != "/":
-            l10n_src = l10n_src + "/"
-    if len(sys.argv) > 2:
-        content_dir = sys.argv[2]
-        if content_dir[-1] != "/":
-            content_dir = content_dir + "/"
-    main()
+    parser = argparse.ArgumentParser(
+        description="Loop Stand-alone Client localization update script")
+    parser.add_argument('--src',
+                        default=os.path.join(os.pardir, "loop-client-l10n", "l10n"),
+                        metavar="path",
+                        help="source path for l10n resources")
+    parser.add_argument('--dst',
+                        default=os.path.join("content", "l10n"),
+                        metavar="path",
+                        help="destination path for l10n resources")
+    parser.add_argument('--index-file',
+                        default=os.path.join("content", os.extsep.join(["index", "html"])),
+                        metavar="name",
+                        help="html file to be updated with the locales list")
+    args = parser.parse_args()
+    main(args.src, args.dst, args.index_file)
 
