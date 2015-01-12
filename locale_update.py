0287aaa075d92efc5eaa5447d08a9ee7b6b8e264
Paul Kerr <paulrkerr@gmail.com>
2015-01-12 09:49:25 -0800

    Switched to function form of print, added default values to help text.

--- locale_update.py
+++ locale_update.py
@@ -11,6 +11,8 @@
 # Changes are then pushed back to loop-client.
 ##
 
+from __future__ import print_function
+
 import argparse
 import io
 import os
@@ -18,11 +20,16 @@ import re
 import shutil
 import sys
 
+#defaults
+DEF_L10N_SRC = os.path.join(os.pardir, "loop-client-l10n", "l10n")
+DEF_L10N_DST = os.path.join("content", "l10n")
+DEF_INDEX_FILE_NAME = os.path.join("content", os.extsep.join(["index", "html"]))
+
 def main(l10n_src, l10n_dst, index_file_name):
-    print "deleting existing l10n content tree:", l10n_dst
+    print("deleting existing l10n content tree:", l10n_dst)
     shutil.rmtree(l10n_dst, ignore_errors=True)
 
-    print "updating l10n tree from", l10n_src
+    print("updating l10n tree from", l10n_src)
 
     def create_locale(src_dir):
         dst_dir = src_dir.replace('_', '-')
@@ -32,13 +39,13 @@ def main(l10n_src, l10n_dst, index_file_name):
     locale_dirs = os.listdir(l10n_src)
     locale_list = [create_locale(x) for x in locale_dirs if x[0] != "." and x != "templates"]
 
-    print "updating locales list in", index_file_name
+    print("updating locales list in", index_file_name)
     with io.open(index_file_name, "r+") as index_file:
         index_html = index_file.read()
 
         new_content = re.sub(
             '<meta name=(["|\'])locales\\1.*? content=(["|\']).*?\\2.*? />',
-            '<meta name="locales" content="' + ", ".join(locale_list) + '" />',
+            '<meta name="locales" content="' + ",".join(locale_list) + '" />',
             index_html, 1, re.M | re.S)
         
         index_file.seek(0)
@@ -49,17 +56,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Loop Stand-alone Client localization update script")
     parser.add_argument('--src',
-                        default=os.path.join(os.pardir, "loop-client-l10n", "l10n"),
+                        default=DEF_L10N_SRC,
                         metavar="path",
-                        help="source path for l10n resources")
+                        help="Source path for l10n resources. Default = " + DEF_L10N_SRC)
     parser.add_argument('--dst',
-                        default=os.path.join("content", "l10n"),
+                        default=DEF_L10N_DST,
                         metavar="path",
-                        help="destination path for l10n resources")
+                        help="Destination path for l10n resources. Default = " + DEF_L10N_DST)
     parser.add_argument('--index-file',
-                        default=os.path.join("content", os.extsep.join(["index", "html"])),
+                        default=DEF_INDEX_FILE_NAME,
                         metavar="name",
-                        help="html file to be updated with the locales list")
+                        help="File to be updated with the locales list. Default = " + DEF_INDEX_FILE_NAME)
     args = parser.parse_args()
     main(args.src, args.dst, args.index_file)
 
